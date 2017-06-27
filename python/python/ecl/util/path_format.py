fd0bdf304b772cf8f65c7c2cf0259c671d23de6a
Joakim Hove <joakim.hove@gmail.com>
2017-06-27 18:14:51 +0200

    Added Python constructor to PathFormat

--- python/python/ecl/util/path_format.py
+++ python/python/ecl/util/path_format.py
@@ -1,14 +1,20 @@
 from cwrap import BaseCClass
 from ecl.util import UtilPrototype
 
+# The path_fmt implementation hinges strongly on variable length
+# argument lists in C - not clear if/how that maps over to Python,
+# this Python class therefor has *very* limited functionality.
+
 
 class PathFormat(BaseCClass):
     TYPE_NAME = "path_fmt"
+    _alloc = UtilPrototype("void* path_fmt_alloc_directory_fmt(char*)", bind = False)
     _str  = UtilPrototype("char* path_fmt_get_fmt(path_fmt)")
     _free = UtilPrototype("void path_fmt_free(path_fmt)") 
     
-    def __init__(self):
-        raise NotImplementedError("Class can not be instantiated directly!")
+    def __init__(self, path_fmt):
+        c_ptr = self._alloc( path_fmt )
+        super(PathFormat, self).__init__(c_ptr)
 
 
     def __str__(self):
